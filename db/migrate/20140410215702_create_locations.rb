7866356fe6481705cdf9a6b35878c38bff2049ab
Jesse Buchanan <jbinto@jbinto.ca>
2014-04-10 18:03:56 -0400

    Add location model/migration

--- db/migrate/20140410215702_create_locations.rb
+++ db/migrate/20140410215702_create_locations.rb
@@ -2,7 +2,7 @@ class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :name
-      t.point :latlon
+      t.point :latlon, :geographic => true
 
       t.timestamps
     end
