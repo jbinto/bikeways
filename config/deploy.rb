d454c84ac650a4beada97ad9f1163067d332a03a
Jesse Buchanan <jbinto@jbinto.ca>
2014-04-14 23:17:35 -0400

    Configure Capistrano.
    See https://github.com/jbinto/rails4-sample-app-capistrano for more details.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,58 +1,37 @@
-# config valid only for Capistrano 3.1
+# Adapted from https://github.com/jbinto/rails4-sample-app-capistrano/blob/master/config/deploy.rb
+
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bikeways'
+set :repo_url, 'https://github.com/jbinto/bikeways.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+set :deploy_user, 'gear'
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+# See https://github.com/capistrano/rbenv#usage for details
+# on rbenv boilerplate.
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.1'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
 
-# Default value for :scm is :git
-# set :scm, :git
+# These are symlinks, so redeploys don't "overwrite" important files.
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# Run Rspec tests before deploying. Nifty.
+# Depends on lib/capistrano/tasks/run_tests.cap
+set :tests, ["spec"]
 
 namespace :deploy do
+  before :deploy, "deploy:check_revision"
+  before :deploy, "deploy:run_tests"
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
-  desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join("tmp/restart.txt")
     end
   end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
