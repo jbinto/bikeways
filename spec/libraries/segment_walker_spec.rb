2092552f873cdd611250234e06c5f1ff5ff191f6
Jesse Buchanan <jbinto@jbinto.ca>
2014-04-21 14:16:20 -0400

    Add (literally) edge cases to SegmentWalker#ordered_segments spec

--- spec/libraries/segment_walker_spec.rb
+++ spec/libraries/segment_walker_spec.rb
@@ -11,13 +11,23 @@ describe SegmentWalker do
   let!(:fifth)  { FactoryGirl.create(:bikeway_segment, :from_intersection_id => 5, :to_intersection_id => 1000) }
   let!(:different_after_second) { FactoryGirl.create(:bikeway_segment, :city_linear_feature_name_id => 50, :from_intersection_id => 1000, :to_intersection_id => 1001) }
 
-  it "should be able to arrange segments of a feature in their natural order" do
+  it "ordered_segments should work from the middle" do
+    test_ordered_segments(third.id)
+  end
 
+  it "ordered_segments should work from the left" do
+    test_ordered_segments(first.id)
+  end
+
+  it "ordered_segments should work from the right" do
+    test_ordered_segments(fifth.id)
+  end
+
+  def test_ordered_segments(segment_id)
     walker = SegmentWalker.new(segment_id: third.id)
-
     actual = walker.ordered_segments
     expected = [first, second, third, fourth, fifth]
-
     expect(actual).to eq(expected)
   end
+
 end
