5976caa41362bb18f8789b947a5c4e19b5794764
Jesse Buchanan <jbinto@jbinto.ca>
2014-04-22 21:06:54 -0400

    Add comments to FeatureWalker

--- lib/feature_walker.rb
+++ lib/feature_walker.rb
@@ -8,10 +8,17 @@ class FeatureWalker
     end
   end
 
+  # Analyzes the feature pointed to by @feature_id, and returns an array containing all contiguous paths.
+  # Each entry in the returned array is itself an array of BikewaySegment records, in the correct sequence*.
+  # For example:
+  #    [[a,b,c], [d,e]]
+  #    where all letters are BikewaySegments;
+  #    a,b,c are one contiguous part of the feature;
+  #    d,e are another contiguous part of the feature.
   def paths
     paths = []
 
-    all_segments = segments_by_feature_id(@feature_id)
+    all_segments = find_segments_by_feature_id(@feature_id)
     unwalked_segments = all_segments
     i=1
     until unwalked_segments.empty? do
@@ -28,6 +35,17 @@ class FeatureWalker
   end
 
   private
+  def find_segments_by_feature_id(id)
+    BikewaySegment.where(:city_linear_feature_name_id => id)
+  end
+
+  # Builds a single path from the list of segments passed in.
+  # The entire `segments` array is passed in for better logging,
+  # though technically, only the first element is considered.
+  # The selection of the first element is arbitrary.
+  #
+  # The caller of this method is responsible for keeping track
+  # of which segments have been visited and which have not.
   def build_path(segments)
     segment = segments.first
 
@@ -46,11 +64,4 @@ class FeatureWalker
     walked_segments
   end
 
-  def all_feature_ids
-    BikewaySegment.pluck(:city_linear_feature_name_id).uniq.sort
-  end
-
-  def segments_by_feature_id(id)
-    BikewaySegment.where(:city_linear_feature_name_id => id)
-  end
 end
