2388fa24a3b4401d674295ef8ab51ba1c73d5d43
Jesse Buchanan <hello@jessebuchanan.ca>
2014-10-18 03:11:43 -0400

    Add 'street_union' complex PostGIS query

--- lib/gis_tools.rb
+++ lib/gis_tools.rb
@@ -6,7 +6,6 @@ class GISTools < ActiveRecord::Base
       # http://stackoverflow.com/a/1285117/19779
       sql = "select st_length(st_geomfromtext(?)::geography) as length;"
       result = self.connection.execute sanitize_sql([sql, geom.as_text])
-      binding.pry
       result.first['length'].to_f
     when :rgeo
       # reference: http://daniel-azuma.com/articles/georails/part-4
@@ -15,4 +14,24 @@ class GISTools < ActiveRecord::Base
       geography.length
     end
   end
+
+  def self.street_union
+    # https://gis.stackexchange.com/questions/16698/join-intersecting-lines-with-postgis
+    sql = 'select
+            (st_dump(geom)).geom as geom,
+            full_street_name,
+            bikeway_type
+          from (
+            select
+              st_linemerge(st_union(geom)) as geom,
+              full_street_name,
+              bikeway_type
+            from bikeway_segments
+            group by "full_street_name", "bikeway_type"
+          ) as street_union'
+
+    result = self.connection.execute sanitize_sql([sql])
+    result.to_a
+  end
+
 end
