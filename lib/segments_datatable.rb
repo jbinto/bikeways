feef1967192352836342136a51082dea79f38867
Jesse Buchanan <hello@jessebuchanan.ca>
2014-10-19 02:01:44 -0400

    Update jquery-datatables adapter and templates

--- lib/segments_datatable.rb
+++ lib/segments_datatable.rb
@@ -11,7 +11,7 @@ class SegmentsDatatable
   def as_json(options = {})
     {
       sEcho: params[:sEcho].to_i,
-      iTotalRecords: BikewaySegment.count,
+      iTotalRecords: Segment.count,
       iTotalDisplayRecords: segments.total_entries,
       aaData: data
     }
@@ -22,23 +22,13 @@ class SegmentsDatatable
     segments.map do |segment|
       [
         link_to(segment.id, segment),
-        ERB::Util.h(segment.city_rid),
-        ERB::Util.h(segment.city_geo_id),
-        ERB::Util.h(segment.city_linear_feature_name_id),
-        ERB::Util.h(segment.city_object_id),
         ERB::Util.h(segment.full_street_name),
-        ERB::Util.h(segment.address_left),
-        ERB::Util.h(segment.address_right),
-        ERB::Util.h(segment.odd_even_flag_left),
-        ERB::Util.h(segment.odd_even_flag_right),
         ERB::Util.h(segment.lowest_address_left),
         ERB::Util.h(segment.lowest_address_right),
         ERB::Util.h(segment.highest_address_left),
         ERB::Util.h(segment.highest_address_right),
-        ERB::Util.h(segment.from_intersection_id),
-        ERB::Util.h(segment.to_intersection_id),
-        ERB::Util.h(segment.street_classification),
-        ERB::Util.h(segment.bikeway_type)
+        ERB::Util.h(segment.bikeway_type),
+        ERB::Util.h(segment.length_m)
       ]
     end
   end
@@ -48,7 +38,7 @@ class SegmentsDatatable
   end
 
   def fetch_segments
-    segments = BikewaySegment.order("#{sort_column} #{sort_direction}")
+    segments = Segment.order("#{sort_column} #{sort_direction}")
     segments = segments.page(page).per_page(per_page)
     if params[:sSearch].present?
       where = '' \
@@ -72,23 +62,13 @@ class SegmentsDatatable
   def sort_column
     columns = [
       "id",
-      "city_rid",
-      "city_geo_id",
-      "city_linear_feature_name_id",
-      "city_object_id",
       "full_street_name",
-      "address_left",
-      "address_right",
-      "odd_even_flag_left",
-      "odd_even_flag_right",
       "lowest_address_left",
       "lowest_address_right",
       "highest_address_left",
       "highest_address_right",
-      "from_intersection_id",
-      "to_intersection_id",
-      "street_classification",
-      "bikeway_type"
+      "bikeway_type",
+      "length_m"
     ]
     columns[params[:iSortCol_0].to_i]
   end
